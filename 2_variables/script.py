9863782fcaf09664cbd39304c6d5c65f85fcd25e
Therachart Wicheanrat <kurozaki.ichiko@gmail.com>
2016-03-07 23:23:23 +0700

    second tutorial variable, type and assigned value

---
+++ 2_variables/script.py
@@ -0,0 +1,19 @@
+#This is comment line style in python
+
+#in python variable is autotype, when you declare variable and assigned value then type will auto detect
+
+#Type Integer
+my_int = 20
+print 'My Int =', my_int
+
+#Type Float
+my_float = 2.2245
+print 'My Float =', my_float
+
+#Type Boolean
+my_boolean = True
+print 'My Boolean =', my_boolean
+
+#Re assigned
+my_int = 500
+print 'Re assigned My Int =', my_int
