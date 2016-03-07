2f7c1279a2e42aec08755f644fca7f3e7c911e97
Therachart Wicheanrat <kurozaki.ichiko@gmail.com>
2016-03-07 23:45:29 +0700

    add basic and operation in python

---
+++ 0_basic/script.py
@@ -0,0 +1,14 @@
+# This is single line comment in python
+"""
+This is multiple line comment in python
+"""
+
+"""
+In Python, whitespace is used to structure code. Whitespace is important, so you have to be careful with how you use it.
+whitespace before line 12 and 13 will make this two line scope in spam function.
+"""
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
---
+++ 3_operation/script.py
@@ -0,0 +1,18 @@
+# Addition
+addition = 72 + 23
+print 'Addition =', addition
+# Subtraction
+subtraction = 108 - 204
+print 'Subtraction =', subtraction
+# Multiplication
+multiplication = 108 * 0.5
+print 'Multiplication =', multiplication
+# Division
+division = 108 / 9
+print 'Division =', division
+# Exponentition
+exponent = 9 ** 3
+print 'Exponentition =', exponent
+# Modulo
+mod = 5 % 3
+print 'Modulo =', mod
